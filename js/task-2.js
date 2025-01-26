b23a5ebc5792edb5eb93ae796ef02f198dc245d2
Andriy Romanov <mgm.agro04@gmail.com>
2025-01-26 16:52:22 +0200

    Task 2. Message formatting

--- js/task-2.js
+++ js/task-2.js
@@ -1,18 +1,25 @@
 `use strict`;
-//Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+//Declare a function formatMessage(message, maxLength), which takes a string (message parameter)
+//and checks its length against the specified maximum length (maxLength parameter).
 
-//Доповни код функції таким чином, що:
+//Add the function code so that:
 
-//Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
-//Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
-
-//Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
+//If the length of the string is equal to or less than maxLength, the function returns the original string unchanged.
+//If the length exceeds maxLength, the function truncates the string to maxLength characters,
+//adds an ellipsis "..." at the end, and returns the truncated version.
+function formatMessage(message, maxLength) {
+  // if (message.length <= maxLength) {
+  //  return message;
+  // } else {
+  //  return message.slice(0, maxLength) + `...`; }
+  return message.length <= maxLength
+    ? message
+    : message.slice(0, maxLength) + `...`;
+}
+//Leave this code for review by a mentor.
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-
-//Залиш цей код для перевірки ментором.
